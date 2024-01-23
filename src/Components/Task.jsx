3b6c25c0cd20788a60a9c9e720121c971e06de2e
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-01-22 23:46:44 -0700

    New Styles
    Created new styling for the page.  This improved the overall look of the page.

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,13 +1,10 @@
-import {useState} from 'react';
-
 const Task = ({name, info, type}) => {
-    const [showInfo, setShowInfo] = useState(false);
     return (
         <article>
             <div className='taskItem'>
                 <h4>{name}</h4>
                 <h5>{info}</h5>
-                <h5>Type: {type}</h5>
+                <h5 className="taskType">Category: {type}</h5>
             </div>
         </article>
     )
